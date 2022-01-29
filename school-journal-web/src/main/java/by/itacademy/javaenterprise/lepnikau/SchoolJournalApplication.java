4d324d9a4d5649c9d3e60f9f0624d570363a7bdf
ViachaslauL <Lotussutol@tut.by>
2022-01-29 08:05:15 +0300

    switched: Spring Boot

---
+++ school-journal-web/src/main/java/by/itacademy/javaenterprise/lepnikau/SchoolJournalApplication.java
@@ -0,0 +1,13 @@
+package by.itacademy.javaenterprise.lepnikau;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolJournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolJournalApplication.class, args);
+	}
+
+}
--- school-journal-web/src/main/java/by/itacademy/javaenterprise/lepnikau/config/ServletInitializer.java
+++ school-journal-web/src/main/java/by/itacademy/javaenterprise/lepnikau/config/ServletInitializer.java
@@ -1,11 +1,9 @@
 package by.itacademy.javaenterprise.lepnikau.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 @Configuration
-@EnableWebMvc
 public class ServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
     @Override
